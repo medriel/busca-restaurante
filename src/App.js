2df6eb7944983ef481ceb47da30a117dce75b6df
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-11-01 08:18:51 -0300

    removendo estilizações iniciais do navegador

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset'; //removendo estilizações iniciais do navegador
+
 import './App.css';
 import Home from './pages/home/index';
 import theme from './theme';
@@ -7,6 +9,7 @@ import theme from './theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Reset />
       <Home />
     </ThemeProvider>
   );
