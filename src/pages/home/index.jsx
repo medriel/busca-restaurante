898964e59e6246d525a833b39d12e2011267bfcd
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-11-02 08:52:50 -0300

    Implementando titulo do carousel

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Search, Logo, Wrapper, Map } from './styles';
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
 import logo from '../../assets/logo.svg'
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
@@ -21,6 +21,7 @@ const Home = () => {
               onChange={(e) => setInputValue(e.target.value)}
             />
           </TextField>
+          <CarouselTitle>Na sua Área</CarouselTitle>
         </Search>
       </Container>
       <Map />
