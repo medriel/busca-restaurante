a715e8cf83b338966d225f7c8175d08d4cd50ece
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-11-13 11:21:48 -0300

    Iniciando a busca por restaurantes próximos

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import Slider from "react-slick";
-import { Container, Carousel, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
+import { Container, Carousel, Search, Logo, Wrapper, CarouselTitle } from './styles';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg'
 import restaurant from '../../assets/restaurante-fake.png';
-import { Card, RestaurantCard, Modal } from '../../components';
+import { Card, RestaurantCard, Modal, Map } from '../../components';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
@@ -51,7 +51,7 @@ const Home = () => {
         <RestaurantCard />
       </Container>
       <Map />
-      <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} />
+      {/* <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} /> */}
     </Wrapper>
   );
 };
